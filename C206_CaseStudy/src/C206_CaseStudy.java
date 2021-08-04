766c2aae4fc6a0dabfb7c4de10da7d0c28dd7fb4
20039775-oliver-lim <87061764+20039775-oliver-lim@users.noreply.github.com>
2021-08-04 22:14:05 +0800

    Update C206_CaseStudy.java

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -5,7 +5,8 @@ public class C206_CaseStudy {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ArrayList<Tuition> tuitionList = new ArrayList<Tuition>();
-		
+		 ArrayList<Student> studentList = new ArrayList<Student>();
+		    studentList.add(new Student("Oliver Lim Yue Xuan", "M", 91992299, "20039775@rp.edu.sg", "04/09/2003", "Singapore", 2021));
 		tuitionList.add(new Tuition(11 , "A113","Math", "Basic", 2, "none", 2021 ));
 		tuitionList.add(new Tuition(12 , "C206","Business", "Advance", 2, "basic must be comepleted", 2021 ));
 		
@@ -91,5 +92,65 @@ public class C206_CaseStudy {
 		tuitionList.remove(tui);
 		System.out.println("Tuition deleted");
 	}
-	
+	//Oliver
+	  public static String retrieveStudent(ArrayList<Student> studentList) {
+	    String output = "";
+	    for (int i = 0; i < studentList.size(); i++) {
+	      output += String.format("%s", studentList.get(i).toString());
+	    }
+	    return output;
+	  }
+	  //Oliver
+	  public static void viewStudent(ArrayList<Student> studentList) {
+	    C206_CaseStudy.setHeader("STUDENT LIST");
+	    String output = String.format("%-20s %-15s %-20s %-25s %-20s %-20s %-15s\n", "NAME", "GENDER", "MOBILE NUMBER",
+	        "EMAIL", "dateofbirth", "COR", "year joined");
+	    output += retrieveStudent(studentList);
+	    System.out.println(output);
+	  }
+	//Oliver
+	  public static Student inputStudent() {
+	    String name = Helper.readString("Enter Student's Name: ");
+	    String gender = Helper.readString("Enter your Gender: ");
+	    int mobile = Helper.readInt("Enter your Mobile number: ");
+	    String email = Helper.readString("Enter your email: ");
+	    String dateofbirth = Helper.readString("Enter your Date of Birth: ");
+	    String CoR = Helper.readString("Enter your country of residence: ");
+	    int yearjoined = Helper.readInt("Enter year that you joined: ");
+
+	    Student S = new Student(name, gender, mobile, email, dateofbirth, CoR, yearjoined);
+	    return S;
+	  }
+	    public static void addStudent(ArrayList<Student> studentList, Student S) {
+
+	      studentList.add(S);
+	      System.out.println("Student Added...");
+	    }
+	  //Oliver
+	    public static Boolean doFindStud(ArrayList<Student> studentList, String j) {
+	      boolean isFound = false;
+	      for (int i = 0; i < studentList.size(); i++) {
+	        if (studentList.get(i).getName() == j) {
+	          studentList.remove(i);
+	          isFound = true;
+	        }
+	      }
+	      return isFound;
+	    }
+	    //Oliver
+	    public static void deleteStudent(ArrayList<Student> studentList) {
+	      String name = Helper.readString("Enter your name to delete: ");
+
+	      for (int i = 0; i <= studentList.size(); i++) {
+	        if (studentList.get(i).getName() == name) {
+
+	          studentList.remove(i);
+	          System.out.println("");
+	        } else {
+	          System.out.println("Name is not found.");
+	        }
+	      }
+
+	    }
+	  }
 }
