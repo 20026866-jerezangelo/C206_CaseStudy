03b60a70e0577830c82470c182c711ba03983223
20015307 <20015307@DESKTOP-HT0QHIV>
2021-08-04 23:08:38 +0800

    done by eugene

---
+++ C206_CaseStudy/src/Admin.java
@@ -0,0 +1,17 @@
+
+public class Admin {
+	private String user;
+	private String pw;
+	
+	public Admin(String user, String pw) {
+	this.user = user;
+	this.pw = pw;
+}
+	public String getUser() {
+		return user;
+	}
+	public String getPw() {
+		return pw;
+	}
+}
+
